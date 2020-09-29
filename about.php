e8cbcc413c4f76fe9449918a192733c055e1f372
Oxxro <66285950+OshiroEiji@users.noreply.github.com>
2020-09-29 21:53:19 +0900

    Add files via upload
    aboutエンドロールv3

--- about.php
+++ about.php
@@ -18,6 +18,7 @@
     <link href="https://fonts.googleapis.com/css?family=Homemade+Apple rel=" stylesheet">
     <link href="https://fonts.googleapis.com/css?family=IM+Fell+DW+Pica+SC rel=" stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Quicksand rel=" stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@1,300&display=swap" rel="stylesheet">
     <!-- アバウト用スタイルシート -->
     <style type="text/css">
         .about-font {
@@ -79,6 +80,7 @@
 
         .soft {
             color: rgb(192, 192, 192);
+            width: 12em;
         }
 
         .floatall {
@@ -88,15 +90,73 @@
             flex-wrap: wrap;
 
         }
+
+        .product-name {
+            font-family: 'Source Code Pro', monospace;
+        }
     </style>
+
+    <script type="text/javascript">
+        // 設定開始（スクロールの動きを設定してください）
+
+        var speed = 20; // スクロールのスピード（1に近いほど速く）
+        var move = 1; // スクロールのなめらかさ（1に近いほどなめらかに）
+
+        // 設定終了
+
+
+        // 初期化
+        var x = 0;
+        var y = 0;
+        var nx = 0;
+        var ny = 0;
+
+        function scroll() {
+
+            window.scrollBy(0, move); // スクロール処理
+
+            var rep = setTimeout("scroll()", speed);
+
+            // スクロール位置をチェック（IE用）
+            if (document.all) {
+
+                x = document.body.scrollLeft;
+                y = document.body.scrollTop;
+
+            }
+            // スクロール位置をチェック（NN用）
+            else if (document.layers || document.getElementById) {
+
+                x = pageXOffset;
+                y = pageYOffset;
+
+            }
+
+            if (nx == x && ny == y) { // スクロールし終わっていたら処理を終了
+                // トップページへ自動遷移
+                // location.href = "http://localhost/sotuken/top.php";
+
+                clearTimeout(rep);
+
+            } else {
+
+                nx = x;
+                ny = y;
+
+            }
+
+        }
+    </script>
 </head>
 
-<body>
+<body onLoad="scroll()">
+    <!-- 音楽再生 -->
+    <!-- <audio src="img/amatsuki.mp3" autoplay loop></audio> -->
     <div class="wrapper">
         <!-- ページトップに戻す描写 -->
         <div id="page_top"><a href="#"></a></div>
         <!-- ローディング画面 -->
-        <script>
+        <!-- <script>
             jQuery(function() {
                 var pagetop = $('#page_top');
                 pagetop.hide();
@@ -116,20 +176,14 @@
             });
         </script>
         <!-- ローディング画面の描写-->
-        <div id="loader-bg">
+        <!-- <div id="loader-bg">
             <img src="img/loading.gif">
         </div>
         <script>
             jQuery(window).on('load', function() {
                 jQuery('#loader-bg').hide();
             });
-        </script>
-        <!-- ページ上部のリスト -->
-        <ul id="menu">
-            <li><a href="http://localhost/sotuken/top.php?movie_title">
-                    <div class="title-font"> Canelé Films</div>
-                </a></li>
-        </ul>
+        </script> -->
 
 
         <body>
@@ -137,12 +191,13 @@
             <br />
             <div class=top_text>
                 <div class="about-font">
-                    Production　BIG,AG<br /><br /><br />Crew<br /><br />
-                    <span class="Postion">Art director</span><span class="Name">Minami</span><br />
-                    <span class="Postion">Video editor</span><span class="Name">Nagata</span><br />
-                    <span class="Postion">Project Planning</span><span class="Name">Oshiro</span><br />
-                    <span class="Postion">Development of SQL</span><span class="Name">Hayase</span><br />
-                    <span class="Postion">Special Thanks</span><span class="Name">???????</span><br /><br /><br />
+                    Production　<span class="product-name" style="border-bottom: solid 2px;">BIG,AG</span></span><br /><br /><br /><br />
+                    Project member<br /><br />
+                    <span class="Postion">Art director</span><span class="Name">Toduka Minami</span><br />
+                    <span class="Postion">Video editor</span><span class="Name">Nagata Kazuma</span><br />
+                    <span class="Postion">Development of SQL</span><span class="Name">Hayase Kaito</span><br />
+                    <span class="Postion">Project Planning</span><span class="Name">Oshiro Eiji</span><br />
+                    <span class="Postion">Special Thanks</span><span class="Name">null</span><br /><br /><br />
                     Development Environment<br /><br />
                     <div class="floatall">
                         <div class="floattest"><span class="soft">Text editor</span><span class="soft-name">Visual Studio Code</span></div>
@@ -156,6 +211,14 @@
                         <div class="floattest"><span class="soft">Reference site</span><span class="soft-name">Qiita</span></div>
                         <div class="floattest"><span class="soft">Source of photo</span><span class="soft-name">GIFER</span></div>
                     </div>
+                    <div class="floatall">
+                        <div class="floattest"><span class="soft">Software platforms</span><span class="soft-name">Github</span></div>
+                        <div class="floattest"><span class="soft">Group ware</span><span class="soft-name">Discord Slack</span></div>
+                    </div>
+                    <div class="floatall">
+                        <div class="floattest"><span class="soft">Library</span><span class="soft-name">jQuery</span></div>
+                        <div class="floattest"><span class="soft">WebGIS</span><span class="soft-name">Google Maps</span></div>
+                    </div>
                     <br /><br /><a href="http://localhost/sotuken/top.php?movie_title"><img src="img/fin.jpg"></a>
                 </div>
             </div>
@@ -167,7 +230,7 @@
     <!-- ページ最下部フッター -->
     <footer>
         <div class=footer>
-            <a href="https://www.hamasen.ac.jp/dept/security/">所属 &copy; R2 HAMAJO security&network</a>
+            <a href="https://www.hamasen.ac.jp/dept/security/">所属 &copy; Hamamatsu Professional Traning College of infomation Technology security&network</a>
     </footer>
 </body>
 
